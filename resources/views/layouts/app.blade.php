71be8bf58589e1eba9312cce21d014fadc73edde
unknown <641702@D74131RS_641702.telkom.co.id>
2018-04-06 22:25:51 +0700

    update dashboard menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -393,14 +393,6 @@
                                 <a href="{{ route('admin.dashboard')}}" aria-expanded="false"><i class="fa fa-circle"></i><span class="hide-menu">Dashboard</span></a>
                             </li>
                             <li>
-                                <a class="has-arrow " href="#" aria-expanded="false"><i class="fa fa-circle"></i><span class="hide-menu">Patient</span></a>
-                                <ul aria-expanded="false" class="collapse">
-                                    <li><a href="{{ route('admin.patient.create')}}">Add Patient</a></li>
-                                    <li><a href="{{ route('admin.patient.list')}}">List Patient</a></li>
-                                    
-                                </ul>
-                            </li>
-                            <li>
                                 <a class="has-arrow " href="#" aria-expanded="false"><i class="fa fa-circle"></i><span class="hide-menu">Registration</span></a>
                                 <ul aria-expanded="false" class="collapse">
                                     <li><a href="{{ route('admin.registration.create')}}">Add Registration</a></li>
@@ -408,8 +400,14 @@
                                     
                                 </ul>
                             </li>
-                            
-                            
+                            <li>
+                                <a class="has-arrow " href="#" aria-expanded="false"><i class="fa fa-circle"></i><span class="hide-menu">Patient</span></a>
+                                <ul aria-expanded="false" class="collapse">
+                                    <li><a href="{{ route('admin.patient.create')}}">Add Patient</a></li>
+                                    <li><a href="{{ route('admin.patient.list')}}">List Patient</a></li>
+                                    
+                                </ul>
+                            </li>
                             <li>
                                 {{--  <a class="has-arrow " href="#" aria-expanded="false"><i class="mdi mdi-arrange-send-backward"></i><span class="hide-menu">Multi level dd</span></a>  --}}
                                 <ul aria-expanded="false" class="collapse">
