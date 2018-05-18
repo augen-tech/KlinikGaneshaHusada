f9f2f832dffbd5f97eb5544a9fcac5f80fa9d17f
januarelsan <januarelsan@gmail.com>
2018-05-18 16:40:52 +0700

    Update view with New Diagnosis Columns

--- resources/views/pages/healthAnalyst/resultLab/create.blade.php
+++ resources/views/pages/healthAnalyst/resultLab/create.blade.php
@@ -24,8 +24,7 @@
                         <thead>
                             <tr>
                                 <th style="text-align:center">No</th>                                                                                                
-                                <th style="text-align:center" style="width:10%">Date </th>                                
-                                {{-- <th>Evidence</th>                                 --}}
+                                <th style="text-align:center" style="width:10%">Date </th>                                                                
                                 <th style="text-align:center">Patient</th>                                                                                            
                                 <th style="text-align:center">Doctor</th>  
                                 <th style="text-align:center">Action</th>                                
@@ -34,32 +33,27 @@
                         
                         <tbody>
 
-                            @foreach($diagnoses as $index => $row)
-                                
-                                <tr>
-                                {{-- <td>{{$index + 1}}</td>                                                                 --}}
-                                <td style="text-align:center">{{$row->id}}</td>
-                                <td style="text-align:center">{{$row->created_at}}</td>
-                                {{-- <td>{{$row->evidence}}</td> --}}
-                                <td>
-                                    {{-- {{$row->registration->patient->name}} --}}
-                                    {{str_pad($row->registration->patient->id,6,"0",STR_PAD_LEFT)}}<br>
-                                    {{$row->registration->patient->name}}
-                                </td>                                
-                                <td>
-                                    {{str_pad($row->registration->doctor->id,6,"0",STR_PAD_LEFT)}}<br>
-                                    {{$row->registration->doctor->name}}
-                                </td>                                
-                                <td style="text-align:center">                                
-                                    <a href="{{ route('healthAnalyst.resultLab.form', $row->id)}}">
-                                        <span><i class="fa fa-plus"></i></span>          
-                                    </a>    
-                                    <a href="{{ Storage::url($row->evidence) }}" target="_blank">
-                                        <span><i class="fa fa-download" ></i></span>
-                                    </a>                                    
-                                </td>
-                                
-                                
+                            @foreach($diagnoses as $index => $row)                                
+                                <tr>                                    
+                                    <td style="text-align:center">{{$index + 1}}</td>
+                                    <td style="text-align:center">{{$row->created_at}}</td>
+                                    
+                                    <td>                                        
+                                        {{str_pad($row->registration->patient->id,6,"0",STR_PAD_LEFT)}}<br>
+                                        {{$row->registration->patient->name}}
+                                    </td>                                
+                                    <td>
+                                        {{str_pad($row->registration->doctor->id,6,"0",STR_PAD_LEFT)}}<br>
+                                        {{$row->registration->doctor->name}}
+                                    </td>                                
+                                    <td style="text-align:center">                                
+                                        <a href="{{ route('healthAnalyst.resultLab.form', $row->id)}}">
+                                            <span><i class="fa fa-plus"></i></span>          
+                                        </a>    
+                                        <a href="{{ Storage::url($row->evidence) }}" target="_blank">
+                                            <span><i class="fa fa-download" ></i></span>
+                                        </a>                                    
+                                    </td>                                                                
                                 </tr>
                                 
                             @endforeach
