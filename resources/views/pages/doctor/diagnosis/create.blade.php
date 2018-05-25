10ee6187a780086504ede2e04ea310b6a3397dd6
raaapiw <muhamadrafia.s@gmail.com>
2018-05-25 19:55:13 +0700

    fix add medicine bug midwife

--- resources/views/pages/doctor/diagnosis/create.blade.php
+++ resources/views/pages/doctor/diagnosis/create.blade.php
@@ -156,8 +156,7 @@
                                     <br>
                                 </div>
                             </div>
-                        </div>
-
+                        
                         <div class="row">
                                 <div class="col-md-6">
                                     <div class="form-group">
